f30b9c4d4e812ee9ed9fe7e45c3fa0e3769d58d4
alade93 <99254792+alade93@users.noreply.github.com>
2022-04-12 01:50:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+// const mernStack = ["MongoDB", "Express", "React", "Node"];
+
+// for (i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// //
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+//
+// for (i = 0; i < mernStack.length; i++) {
+//   console.log([i], mernStack[3]);
+// }
+//
+// for (let i = 0; i <= 1; i++) {
+//   console.log(i);
+// }
+
+//
+// for (i = 0; i <= 7; i++) {
+//   for (j = 0; j <= 1; j++) {
+//     for (j = 1; j <= 2; j++) {
+//       for (j = 1; j <= 3; j++) {
+//         for (j = 1; j <= 4; j++) {
+//           for (j = 1; j <= 5; j++) {
+//             for (j = 1; j <= 6; j++) {
+//               for (j = 1; j <= 7; j++) {
+//                 console.log(i);
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+//
+// for (let i = 0; i <= 100; i++) {
+//   console.log(`${i} * ${i} = ${i * i}`);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${1} * ${i} = ${1 * i}`);
+// }
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${i} * ${i} = ${i * i}`);
+// }
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${i} * ${i} * ${i} = ${i * i * i}`);
+// }
+//
+// let sum = 0;
+// for (let i = 0; i < 101; i++) {
+//   console.log(i);
+//   sum = sum + i;
+// }
+// console.log(sum);
+// let sum = 0;
+// for (let i = 1; i < 101; i = i + 2) {
+//   console.log(1);
+//   sum = sum + i;
+// }
+// console.log(sum); //odd numbers
+
+// let sum = 0;
+// for (let i = 0; i < 101; i = i + 2) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum); //even numbers
+
+// const randomNum = [];
+// for (let i = 0; i <= 5; i++) {
+//   randomNum.push(Math.ceil(Math.random() * 5));
+// }
+// console.log(randomNum);//array of random numbers
+
+// const ranNum = [];
+// for (i = 0; i <= 5; i++) {
+//   ranNum.push(Math.ceil(Math.random() * 5)).toString(5);
+// }
+// console.log(ranNum);
+
+// const id = Math.random().toString(36).substring(6);
+// console.log(id);//random id
